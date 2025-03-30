c07516ec8133078f59f1de49717701c7b955c3dd
MapleQiAN <serendylin@outlook.com>
2025-03-30 20:53:16 +0800

    Site updated: 2025-03-30 20:53:10

---
+++ githubcalendar/index.js
@@ -0,0 +1,10 @@
+home_widget:
+  - class_name: my_github_container
+    id_name: my_github_container
+    order: -1
+    name: Github日历
+    icon: fa-solid fa-calendar-days
+    icon_color: '#42b983'
+    url: https://github.com/mapleqian
+    background: '#fff'
+    html: '<img no-lazy width="100%" hright="300px" src="https://ghchart.rshah.org/42b983/MapleQiAN" alt="MapleQiAN github" />'
